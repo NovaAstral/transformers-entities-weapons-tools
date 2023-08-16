b0eb6d3dea576d91b98ac2106818b5ca776fabdf
NovaAstral <mariofluddd@gmail.com>
2023-08-16 20:55:22 -0300

    delete non-functional halo

--- lua/entities/autorun/ground_bridge_halo.lua
+++
@@ -1,6 +0,0 @@
-AddCSLuaFile()
-if CLIENT then
-    hook.Add( "PreDrawHalos", "GroundBridgeHalo", function()
-	    halo.Add(ents.FindByClass("ground_bridge_portal"),Color(0,255,158),2,2,2,true,false)
-    end)
-end
